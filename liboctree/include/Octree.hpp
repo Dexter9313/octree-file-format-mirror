81c20f215d8eb0ff1a8024cc195d284c09641a49
Florian Cabot <florian.cabot@epfl.ch>
2018-12-10 15:12:41 +0100

    Added documentation generation with doxygen

--- liboctree/include/Octree.hpp
+++ liboctree/include/Octree.hpp
@@ -24,6 +24,11 @@
 class Octree : public Leaf
 {
   public:
+	  /*! Data constructor
+	   *
+	   * Constructs an octree from cartesian positions.
+	   * \param data Vector of the form {x0, y0, z0, x1, y1, z1, ... , xn, yn, zn}.
+	   */
 	Octree(std::vector<float> data);
 	Octree(std::istream& in);
 	virtual std::vector<long> getCompactData() const;
