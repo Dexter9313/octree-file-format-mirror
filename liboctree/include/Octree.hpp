efc773dbab1e1e83b0a2e4be9a77608e5af93c90
Florian Cabot <florian.cabot@epfl.ch>
2018-12-17 15:01:31 +0100

    Wrote liboctree documentation

--- liboctree/include/Octree.hpp
+++ liboctree/include/Octree.hpp
@@ -30,37 +30,167 @@
 
 #define MAX_LEAF_SIZE 16000
 
+/*! \brief Octree main class
+ */
 class Octree
 {
   public:
+	/*! \brief Initializes the octree from position data.
+	 *
+	 * It will also compute all the mins and maxes.
+	 *
+	 * \param data : vector holding positions, structured as follows for N
+	 * points : {x1, y1, z1, ... xN, yN, zN}.
+	 */
 	virtual void init(std::vector<float> data);
+
+	/*! \brief Initializes the octree from a stream.
+	 *
+	 * The tree will only read its structure and not its data. To read the data,
+	 * call \ref readData afterwards.
+	 *
+	 * \param in : stream from which to read the structure
+	 */
 	virtual void init(std::istream& in);
+
+	/*! \brief Initializes the octree from a address within a file.
+	 *
+	 * Typically called by init(std::istream&) when reading the structure if
+	 * this octree to be created is a leaf.
+	 * No children will be created, only the file address will be set.
+	 *
+	 * \param file_addr : Address within the file where the data lies.
+	 */
 	virtual void init(long file_addr);
+
+	/*! \brief Tests if this node is in fact a leaf.
+	 *
+	 * More explicitly, returns true if and only if all elements of \ref
+	 * children are nullptr.
+	 */
 	bool isLeaf() const;
+
+	/*! \brief Returns a "string" of long ints that represents the octree's
+	 * structure
+	 *
+	 * This vector complies to the octree file format and can be written as is
+	 * to represent a TREE from the grammar. If this is the root node, don't
+	 * forget not to write the size of the vector and the first parenthesis to
+	 * fully comply to the format.
+	 */
 	virtual std::vector<long> getCompactData() const;
+
+	/*! \brief Writes the data within the stream and updates file_addr
+	 * accordingly.
+	 *
+	 * It will write all min/maxes and the position data.
+	 * \param out : stream to which to write
+	 */
 	virtual void writeData(std::ostream& out);
+
+	/*! \brief Reads data at file_addr from a stream
+	 *
+	 * It will read all min/maxes and the position data.
+	 * \param in : stream from which to read
+	 */
 	virtual void readData(std::istream& in);
+
+	/*! \brief Returns all the position data contained within the whole octree.
+	 */
 	virtual std::vector<float> getAllData() const;
+
+	/*! \brief Returns a displayable string to represent the tree.
+	 */
 	virtual std::string toString(std::string const& tabs = "") const;
+
+	/*! \brief Destructor.
+	 *
+	 * Deletes children if any.
+	 */
 	virtual ~Octree();
 
   protected:
+	/*! \brief Constructs a new octree.
+	 *
+	 * Useful for inheritence and polymorphism. You SHOULD reimplement this
+	 * method if you inherit from Octree to return your own instance of it. This
+	 * is needed due to the recursive nature of this class. When Octree
+	 * initializes itself, it will call newOctree() to create children to ensure
+	 * the whole tree will be of the right class.
+	 *
+	 * Example : inheriting from Octree
+	 * @code
+	 * class MyOctree : public Octree
+	 * {
+	 *    // ... some members ...
+	 *    protected:
+	 *      virtual Octree* newOctree() const { return new MyOctree(); };
+	 * }
+	 * @endcode
+	 */
 	virtual Octree* newOctree() const;
 
+	/*! \brief Address within a file where lies or should lie the data.
+	 */
 	long file_addr = -2;
 
+	/*! \brief Min value of the positions' x component.
+	 *
+	 * Part of the data read/written.
+	 */
 	float minX = FLT_MAX;
+	/*! \brief Max value of the positions' x component.
+	 *
+	 * Part of the data read/written.
+	 */
 	float maxX = -FLT_MAX;
+	/*! \brief Min value of the positions' y component.
+	 *
+	 * Part of the data read/written.
+	 */
 	float minY = FLT_MAX;
+	/*! \brief Max value of the positions' y component.
+	 *
+	 * Part of the data read/written.
+	 */
 	float maxY = -FLT_MAX;
+	/*! \brief Min value of the positions' z component.
+	 *
+	 * Part of the data read/written.
+	 */
 	float minZ = FLT_MAX;
+	/*! \brief Max value of the positions' z component.
+	 *
+	 * Part of the data read/written.
+	 */
 	float maxZ = -FLT_MAX;
 
+	/*! \brief Position data.
+	 *
+	 * Part of the data read/written.
+	 *
+	 * This vector is structured as follows for N points : {x1, y1, z1, ... xN,
+	 * yN, zN}.
+	 */
 	std::vector<float> data;
+
+	/*! \brief Children of this node.
+	 *
+	 * If this node is a leaf, all the children will be nullptr.
+	 */
 	Octree* children[8] = {nullptr, nullptr, nullptr, nullptr,
-	                     nullptr, nullptr, nullptr, nullptr};
+	                       nullptr, nullptr, nullptr, nullptr};
 };
 
+/*! \brief Writes an Octree in a stream.
+ *  \relates Octree
+ *
+ * It will write the whole file structure (tree structure + data chunks) within
+ * the stream according to the octree file format.
+ *
+ * \param stream : stream in which to write
+ * \param octree : octree to be written
+ */
 void write(std::ostream& stream, Octree& octree);
 
 #endif // OCTREE_H
--- liboctree/include/binaryrw.hpp
+++ liboctree/include/binaryrw.hpp
@@ -23,27 +23,124 @@
 #include <iostream>
 #include <vector>
 
-/*! Binary read-write.
+/*! \brief Binary read-write.
+ *
+ * This namespace contains simple functions to read and write binary data from
+ * and to a file.
+ *
+ * Reads and writes are supposed to be used together : some data written with
+ * one of these "write" functions should be read with its correponding "read"
+ * function. Other usage behaviors are possible but risky, be cautious !
  */
 namespace brw
 {
 // DECLARATIONS
-/*! Returns true if and only if the machine this function is running on is little endian.
+/*! \brief Returns true if and only if the machine this function is running on
+ * is
+ * little endian.
  */
 inline bool isLittleEndian();
 
+/*! \brief Writes some base-type data.
+ *
+ * Example 1 : writing an integer to a file
+ * @code
+ * std::ofstream f("/path", std::fstream::out | std::fstream::binary);
+ * int x(42);
+ * brw::write(f, x);
+ * f.close();
+ * @endcode
+ *
+ * Example 2 : writing an integer buffer to a file
+ * @code
+ * std::ofstream f("/path", std::fstream::out | std::fstream::binary);
+ * int x[2] = {42, 43};
+ * brw::write(f, x[0], 2);
+ * f.close();
+ * @endcode
+ *
+ * \param stream : the stream in which to write
+ * \param x : the buffer of base-type T to write
+ * \param n : if x is the first element of a buffer, which is the number of
+ * elements to write
+ */
 template <typename T>
 inline void write(std::ostream& stream, T& x, unsigned int n = 1);
 
+/*! \brief Reads some base-type data.
+ *
+ * Example 1 : reading an integer from a file
+ * @code
+ * std::ifstream f("/path", std::fstream::in | std::fstream::binary);
+ * int x;
+ * brw::read(f, x);
+ * // x now holds the read value
+ * f.close();
+ * @endcode
+ *
+ * Example 2 : reading an integer buffer from a file
+ * @code
+ * std::ifstream f("/path", std::fstream::in | std::fstream::binary);
+ * int x[2];
+ * brw::read(f, x[0], 2);
+ * // x now holds the two read values
+ * f.close();
+ * @endcode
+ *
+ * \param stream : the stream from which to read
+ * \param res : the data of base-type T to write into once it's read
+ * \param n : if res is the first element of a buffer, which is the number of
+ * elements to read
+ */
 template <typename T>
 inline void read(std::istream& stream, T& res, unsigned int n = 1);
 
+/*! \brief Writes a vector of base-type.
+ *
+ * The size of the vector will also be written, which is useful for reading.
+ *
+ * Example : writing a vector of integers to a file
+ * @code
+ * std::ofstream f("/path", std::fstream::out | std::fstream::binary);
+ * std::vector<int> v = {1, 2, 3};
+ * brw::write(f, v);
+ * f.close();
+ * @endcode
+ *
+ * \param stream : the stream in which to write
+ * \param array : the vector of base-type T to write
+ */
 template <typename T>
 inline void write(std::ostream& stream, std::vector<T>& array);
 
+/*! \brief Reads a vector of base-type.
+ *
+ * The size of the vector will be read from the file.
+ *
+ * Example : reading a vector of integers from a file
+ * @code
+ * std::ifstream f("/path", std::fstream::in | std::fstream::binary);
+ * std::vector<int> v;
+ * brw::read(f, v);
+ * // v now holds the read vector
+ * f.close();
+ * @endcode
+ *
+ * \param stream : the stream from which to read
+ * \param vec : the vector of base-type T in which to write the read data
+ */
 template <typename T>
 inline void read(std::istream& stream, std::vector<T>& vec);
 
+/*! \brief \brief Reads a whole stream and writes the hex values found to
+ * another.
+ *
+ * It will read \p stream char by char and write their corresponding hex values
+ * one by one.
+ *
+ * \param stream : the stream from which to read the data
+ * \param dbgstream : the stream to which to write the hex values
+ */
 inline void debugread(std::istream& stream, std::ostream& dbgstream);
 
 // DEFINITIONS
@@ -101,6 +198,7 @@ void debugread(std::istream& stream, std::ostream& dbgstream)
 	}
 	dbgstream << std::endl;
 }
+
 } // ns brw
 
 #endif // BINARYRW_H
