d8b4952faee8e621f55e92cc6c112c4f845d6fbf
Florian Cabot <florian.cabot@epfl.ch>
2019-03-13 14:22:24 +0100

    Added some progress bars, more to come

--- octreegen/include/utils.hpp
+++ octreegen/include/utils.hpp
@@ -26,3 +26,4 @@ std::vector<std::string> split(std::string const& str, char c = ' ');
 std::string join(std::vector<std::string> const& strs, char c = ' ');
 std::vector<float> generateVertices(unsigned int number, unsigned int seed);
 std::vector<float> readHDF5(std::string const& path, const char* parttype);
+void showProgress(float progress);
